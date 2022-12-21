5db06afeaa37edd4153664745d4dd654d7797af8
Abdrahman Moussa <maloom7>
2022-12-22 00:01:50 +0300

    5 add doctor

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,26 +2,8 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Corona Admin</title>
-    <!-- plugins:css -->
-    <link rel="stylesheet" href="/public/admin/assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="/public/admin/assets/vendors/css/vendor.bundle.base.css">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="/public/admin/assets/vendors/jvectormap/jquery-jvectormap.css">
-    <link rel="stylesheet" href="/public/admin/assets/vendors/flag-icon-css/css/flag-icon.min.css">
-    <link rel="stylesheet" href="/public/admin/assets/vendors/owl-carousel-2/owl.carousel.min.css">
-    <link rel="stylesheet" href="/public/admin/assets/vendors/owl-carousel-2/owl.theme.default.min.css">
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <!-- endinject -->
-    <!-- Layout styles -->
-    <link rel="stylesheet" href="/public/admin/assets/css/style.css">
-    <!-- End layout styles -->
-    <link rel="shortcut icon" href="/public/admin/assets/images/favicon.png" />
+    @include('admin.css')
+
   </head>
   <body>
     <div class="container-scroller">
@@ -64,25 +46,8 @@
 
     <!-- container-scroller -->
     <!-- plugins:js -->
-    <script src="/public/admin/assets/vendors/js/vendor.bundle.base.js"></script>
     <!-- endinject -->
-    <!-- Plugin js for this page -->
-    <script src="/public/admin/assets/vendors/chart.js/Chart.min.js"></script>
-    <script src="/public/admin/assets/vendors/progressbar.js/progressbar.min.js"></script>
-    <script src="/public/admin/assets/vendors/jvectormap/jquery-jvectormap.min.js"></script>
-    <script src="/public/admin/assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-    <script src="/public/admin/assets/vendors/owl-carousel-2/owl.carousel.min.js"></script>
-    <script src="/public/admin/assets/js/jquery.cookie.js" type="text/javascript"></script>
-    <!-- End plugin js for this page -->
-    <!-- inject:js -->
-    <script src="/public/admin/assets/js/off-canvas.js"></script>
-    <script src="/public/admin/assets/js/hoverable-collapse.js"></script>
-    <script src="/public/admin/assets/js/misc.js"></script>
-    <script src="/public/admin/assets/js/settings.js"></script>
-    <script src="/public/admin/assets/js/todolist.js"></script>
-    <!-- endinject -->
-    <!-- Custom js for this page -->
-    <script src="/public/admin/assets/js/dashboard.js"></script>
+    @include('admin.script')
     <!-- End custom js for this page -->
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
+use App\Http\Controllers\AdminController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -34,3 +37,9 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+// Add doctors
+Route::get('/add_doctor_view',[AdminController::class,'addview']);
+
+Route::post('/upload_doctor',[AdminController::class,'upload']);
+
